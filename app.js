e64c879961db5443dc1f7fbda4910533693da9e3
simotasca <simo.tasca@gmail.com>
2021-12-05 16:36:17 +0100

    serve homepage

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const path = require('path');
 
 const app = express()
 const port = process.env.port || 3000;
 
 app.use(express.static('public'));
+
+// sendFile will go here
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname, '/index.html'));
+});
 
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`)
