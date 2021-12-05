145e4a802bf6e2a4ea3999729659b85c75bc16fa
simotasca <simo.tasca@gmail.com>
2021-12-05 17:09:00 +0100

    port problem fix

--- app.js
+++ app.js
@@ -3,13 +3,10 @@ const path = require('path');
 
 const app = express()
 
-//app.use(express.static('public'));
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
 	res.send("CIAOOOOOO");
 });
 
-const port = process.env.port || 3000;
-app.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(process.env.PORT || 3000);
