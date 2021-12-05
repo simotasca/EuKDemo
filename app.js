13989acd81bc7df4bc353e26f1c00ef3747aae4e
simotasca <simo.tasca@gmail.com>
2021-12-05 16:59:03 +0100

    prove heroku

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ const express = require("express");
 const path = require('path');
 
 const app = express()
-const port = process.env.port || 3000;
 
-app.use(express.static('public'));
+//app.use(express.static('public'));
 
-// sendFile will go here
 app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, '/index.html'));
+	res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
+const port = process.env.port || 3000;
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`)
 })
