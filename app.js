9305b84b7fb29ba402cfeb31f789cc4c79d04ae4
simotasca <simo.tasca@gmail.com>
2021-12-06 15:36:06 +0100

    server error handling

--- app.js
+++ app.js
@@ -6,7 +6,14 @@ const app = express()
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-	res.send("CIAOOOOOO");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(process.env.PORT || 3000);
+// error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  next(err);
+})
+
+let port = process.env.PORT || 3000;
+app.listen(port, () => console.log("server listen at http://localhost:%d", port));
