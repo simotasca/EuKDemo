29b0ea8d7d215d186152675dfaffe067d0f04021
simotasca <simo.tasca@gmail.com>
2022-06-29 16:18:15 +0200

    Revolusion

--- js/pages/index/main-animation.js
+++ js/pages/index/main-animation.js
@@ -33,42 +33,65 @@ function nextProduct() {
   if (currentProduct == totProducts)
     currentProduct = 0
 
-  document.querySelector(`#product-img-${currentProduct}`).classList.remove('d-none')
-  document.querySelector(`#product-side-img-${currentProduct}`).classList.remove('d-none')
-  document.querySelector(`#hero-${currentProduct}`).classList.remove('d-none')
-  document.querySelector(`#firma-rav-${currentProduct}`).classList.remove('d-none')
+  document.querySelector(`#product-${currentProduct}`).classList.remove('d-none')
+  document.querySelector(`#side-${currentProduct}`).classList.remove('d-none')
+  document.querySelector(`#firma-${currentProduct}`).classList.remove('d-none')
 
   document.querySelector(`#ellipse-${currentProduct}`).classList.remove('d-none')
-  document.querySelector(`#ellipse-${lastProduct}`).classList.remove('main-ellipse')
-  document.querySelector(`#ellipse-${currentProduct}`).classList.add('main-ellipse')
+  document.querySelector(`#ellipse-${lastProduct}`).classList.remove('ellipse-bg--top')
+  document.querySelector(`#ellipse-${currentProduct}`).classList.add('ellipse-bg--top')
 
 
   let timeline = gsap.timeline({
     onComplete: () => {
-      document.querySelector(`#product-img-${lastProduct}`).classList.add('d-none')
-      document.querySelector(`#product-side-img-${lastProduct}`).classList.add('d-none')
-      document.querySelector(`#hero-${lastProduct}`).classList.add('d-none')
-      document.querySelector(`#firma-rav-${lastProduct}`).classList.add('d-none')
+      document.querySelector(`#product-${lastProduct}`).classList.add('d-none')
+      document.querySelector(`#side-${lastProduct}`).classList.add('d-none')
+      document.querySelector(`#firma-${lastProduct}`).classList.add('d-none')
       document.querySelector(`#ellipse-${lastProduct}`).classList.add('d-none')
       animating = false
       !timout && settalo()
     }
   })
 
+  if (window.innerWidth >= 950) {
+    timeline
+      .fromTo(`#product-${lastProduct}`, { x: 0, rotation: 0 }, { x: '-100%', rotation: -10, duration: 1, ease: "Power2.easeIn" }, 0.4)
+      .fromTo(`#product-${currentProduct}`, { x: '100%', rotation: 10 }, { x: 0, rotation: 0, duration: 1, ease: "Power2.easeOut" }, 1.1)
+
+      .fromTo(`#side-${lastProduct}`, { x: 0, rotation: 0 }, { x: '-100%', duration: 1, ease: "Power2.easeIn" }, 0.5)
+      .fromTo(`#side-${currentProduct}`, { x: '100%' }, { x: 0, rotation: 0, duration: 1, ease: "Power2.easeOut" }, 1.2)
+
+      .fromTo(`#firma-${lastProduct}`, { x: 0, autoAlpha: 1 }, { x: '-50', autoAlpha: 0, duration: 0.7 }, 0.3)
+      .fromTo(`#firma-${currentProduct}`, { x: "50", autoAlpha: 0 }, { x: 0, autoAlpha: 1, duration: 0.7 }, 1.25)
+
+      .fromTo(`#hero-${lastProduct}`, { x: 0, autoAlpha: 1 }, { x: '-50', autoAlpha: 0, duration: 0.7 }, 0.2)
+      .add(() => {
+        // va fatto qui perchè uso la position relative
+        document.querySelector(`#hero-${currentProduct}`).classList.remove('d-none')
+        document.querySelector(`#hero-${lastProduct}`).classList.add('d-none')
+        document.querySelector(`#contatti`).className = `contatti-${currentProduct}`
+      }, 1)
+      .fromTo(`#hero-${currentProduct}`, { x: "50", autoAlpha: 0 }, { x: 0, autoAlpha: 1, duration: 0.7 }, 1.15)
+
+      .fromTo(`#ellipse-${currentProduct}`, { x: '100%' }, { x: 0, duration: 1 }, 0.9)
+
+    return
+  }
+
+
+
   timeline
-    .fromTo(`#product-img-${lastProduct}`, { x: 0, rotation: 0 }, { x: '-100%', rotation: -10, duration: 1, ease: "Power2.easeIn" }, 0.4)
-    .fromTo(`#product-img-${currentProduct}`, { x: '100%', rotation: 10 }, { x: 0, rotation: 0, duration: 1, ease: "Power2.easeOut" }, 1.1)
-    
-    .fromTo(`#product-side-img-${lastProduct}`, { x: 0, rotation: 0 }, { x: '-100%', duration: 1, ease: "Power2.easeIn" }, 0.5)
-    .fromTo(`#product-side-img-${currentProduct}`, { x: '100%' }, { x: 0, rotation: 0, duration: 1, ease: "Power2.easeOut" }, 1.2)
-   
-    .fromTo(`#firma-rav-${lastProduct}`, { x: 0, autoAlpha: 1 }, { x: '-50', autoAlpha: 0, duration: 0.7 }, 0.3)
-    .fromTo(`#firma-rav-${currentProduct}`, { x: "50", autoAlpha: 0 }, { x: 0, autoAlpha: 1, duration: 0.7 }, 1.25)
+    .fromTo(`#hero-${lastProduct}`, { x: 0, autoAlpha: 1 }, { x: '-20', autoAlpha: 0, duration: 0.4 }, 0.2)
+    .fromTo(`#product-group-${lastProduct}`, { x: 0 }, { x: '-100%', duration: 1, ease: "Power2.easeIn" }, '>')
+    .add(() => {
+      // va fatto qui perchè uso la position relative
+      document.querySelector(`#hero-${currentProduct}`).classList.remove('d-none')
+      document.querySelector(`#hero-${lastProduct}`).classList.add('d-none')
+      document.querySelector(`#contatti`).className = `contatti-${currentProduct}`
+    }, '>')
+    .fromTo(`#hero-${currentProduct}`, { x: "20", autoAlpha: 0 }, { x: 0, autoAlpha: 1, duration: 0.7 }, '>')
+    .fromTo(`#product-group-${currentProduct}`, { x: '100%' }, { x: 0, duration: 1, ease: "Power2.easeOut" }, 1.9)
+    .fromTo(`#ellipse-${currentProduct}`, { x: '100%' }, { x: 0, duration: 2 }, 0.9)
 
-    .fromTo(`#hero-${lastProduct}`, { x: 0, autoAlpha: 1 }, { x: '-50', autoAlpha: 0, duration: 0.7 }, 0.2)
-    .fromTo(`#hero-${currentProduct}`, { x: "50", autoAlpha: 0 }, { x: 0, autoAlpha: 1, duration: 0.7 }, 1.15)
 
-    .fromTo(`#ellipse-${currentProduct}`, { x: '100%' }, { x: 0, duration: 1 }, 0.9)
-  // .fromTo('#ellipse-1', { y: '100%' }, { y: 0, duration: 1 }, 0)
-  // .fromTo('#product-img-1', { y: '100%' }, { y: 0, duration: 1 }, 0.1)
 }
