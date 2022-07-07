a0fa7f9e9f147029ea21a2962d66ac5fb0699863
simotasca <simo.tasca@gmail.com>
2022-07-07 15:26:46 +0200

    PERCORSI SLASH

--- js/components/main-menu.js
+++ js/components/main-menu.js
@@ -1,4 +1,8 @@
 class MainMenu extends BaseComponent {
+
+  lastScrollY
+  isOpen = false
+
   #onWindowScroll() {
     const element = this.qSelect('#main-menu')
     // const conts = document.querySelector('#contatti-container')
@@ -26,10 +30,10 @@ class MainMenu extends BaseComponent {
         // conts.classList.remove('scrolled')
       }
 
-      if (window.scrollY > (document.body.clientHeight - 20)) {
-        element.style.transform = 'translateY(0)'
-        // conts.classList.remove('scrolled')
-      }
+      // if (window.scrollY > (document.body.clientHeight - 20)) {
+      //   element.style.transform = 'translateY(0)'
+      //   // conts.classList.remove('scrolled')
+      // }
     }
 
     this.lastScrollY = window.scrollY
@@ -40,13 +44,13 @@ class MainMenu extends BaseComponent {
 
     this.setTemplate(`
       <style>
-        @import "css/components/main-menu.css";
+        @import "/css/components/main-menu.css";
       </style>
 
       <header id="main-menu" class="${this.hasAttribute('dark') && 'main-menu--dark'}">
         <div id="background"></div>
         <div class="navbar max-width-container">
-          <img src="./resources/img/logo.svg" alt="" ${this.hasAttribute('noLogo') && 'style="visibility: hidden;"'}>
+          <img src="/resources/img/logo.svg" alt="" ${this.hasAttribute('noLogo') && 'style="visibility: hidden;"'}>
           <nav class="only-on-desktop">
             <a href="/">Homepage</a>
             <a href="/lista-prodotti.html">Lista prodotti</a>
@@ -55,14 +59,13 @@ class MainMenu extends BaseComponent {
             <a href="/blog.html" id="menu-editoriale">Editoriale</a>
             <div id="menu-underline"></div>
           </nav>
-          <img id="menu-hamb" class="hide-on-desktop" src="./resources/img/icons/hamburger.svg" alt="">
+          <!--<img id="menu-hamb" class="hide-on-desktop" src="/resources/img/icons/hamburger.svg" alt="">-->
+          <object id="menu-hamb" class="hide-on-desktop pointer" type="image/svg+xml" data="/resources/img/icons/hamburger.svg"></object>
         </div>
         <div id="extra" class="max-width-container"><slot/></div>
       </header>`)
 
     this.lastScrollY = 0
-
-
 
     if (this.hasAttribute('noTransparent')) {
       this.qSelect('#main-menu').classList.add('scrolled')
@@ -71,6 +74,20 @@ class MainMenu extends BaseComponent {
 
     this.#onWindowScroll()
     window.addEventListener("scroll", this.#onWindowScroll.bind(this))
+    this.qSelect('img').addEventListener('click', this.hambClick.bind(this))
+
+  }
+
+  hambClick() {
+    let svgDoc = this.qSelect("#menu-hamb").contentDocument
+    let line1 = svgDoc.getElementById('line-1')
+    let line2 = svgDoc.getElementById('line-2')
+    let line3 = svgDoc.getElementById('line-3')
+    if(!this.isOpen) {
+      line1.classList.add('line-1--open')
+      line2.classList.add('line-2--open')
+      line3.classList.add('line-3--open')
+    }
   }
 }
 
